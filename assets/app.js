5705a2afaad9251d2f420806125d425bcef2dd59
mnjo <100378645+MLB-78@users.noreply.github.com>
2024-10-18 23:21:46 +0200

    AFRICAN KINGDOM MWANA M'BOKA

--- assets/app.js
+++ assets/app.js
@@ -2,13 +2,8 @@ import './bootstrap.js';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '@popperjs/core';
-/*
- * Welcome to your app's main JavaScript file!
- *
- * This file will be included onto the page via the importmap() Twig function,
- * which should already be in your base.html.twig.
- */
 import './styles/app.css';
+
 
 setTimeout(function() {
     let alert = document.querySelector('.alert');
